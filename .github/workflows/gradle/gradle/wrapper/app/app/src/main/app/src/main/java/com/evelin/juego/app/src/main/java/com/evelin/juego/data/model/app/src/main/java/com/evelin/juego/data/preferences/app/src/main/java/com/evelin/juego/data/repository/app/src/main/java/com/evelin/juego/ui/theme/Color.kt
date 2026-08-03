bc61afea71e62692e3c68573a4bea151d2dab969
marianoparrot <maildemariano@gmail.com>
2026-08-03 21:20:09 +0200

    Create Color.kt

---
+++ .github/workflows/gradle/gradle/wrapper/app/app/src/main/app/src/main/java/com/evelin/juego/app/src/main/java/com/evelin/juego/data/model/app/src/main/java/com/evelin/juego/data/preferences/app/src/main/java/com/evelin/juego/data/repository/app/src/main/java/com/evelin/juego/ui/theme/Color.kt
@@ -0,0 +1,13 @@
+package com.evelin.juego.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val BurgundyDark = Color(0xFF1A050B)
+val BurgundyMid = Color(0xFF3B0B18)
+val BurgundyLight = Color(0xFF6B112B)
+val WineRed = Color(0xFF8B0000)
+val RoseGold = Color(0xFFE8C39E)
+val SoftPink = Color(0xFFFFD1DC)
+val TextWhite = Color(0xFFFBF8F6)
+val CardBackground = Color(0x333B0B18)
+val BorderWine = Color(0x668B0000)
